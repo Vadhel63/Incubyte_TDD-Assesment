bc902ef7ed5e96500e0535f10cbc27c971592512
Vadhel63 <vadhelmilan45@gmail.com>
2025-07-04 22:35:00 +0530

    [Add test-3] Added Failing Test for string can contain NewLine As Delimiter

--- src/test/java/org/VadhelMilan/StringCalculatorTest.java
+++ src/test/java/org/VadhelMilan/StringCalculatorTest.java
@@ -35,5 +35,11 @@ public class StringCalculatorTest{
         assertEquals(29,stringCalculator.Add("5,7,8,9"));
 
     }
+    @Test
+    public void  NewLine_As_Delimiter_Must_Work()
+    {
+        assertEquals(6,stringCalculator.Add("4\n2"));
+        assertEquals(7,stringCalculator.Add("2/n4,1"));
+    }
 
 }
