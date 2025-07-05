1b15f130744493a70ed048a3f9f942c668f276f0
Vadhel63 <vadhelmilan45@gmail.com>
2025-07-05 16:51:08 +0530

    [Add test-10] Passed : Test For Multiple Delimiter with Single Char Should Work

--- src/main/java/org/VadhelMilan/StringCalculator.java
+++ src/main/java/org/VadhelMilan/StringCalculator.java
@@ -2,6 +2,7 @@ package org.VadhelMilan;
 
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
@@ -37,6 +38,18 @@ public class StringCalculator
             throw new IllegalArgumentException("negative not allowed: " + String.join(",", neg));
         }
     }
+    public int CountChar(String Numbers,char target)
+    {
+        int CountChar=0;
+        for(int i=0;i<Numbers.length();i++)
+        {
+            if(Numbers.charAt(i)==target)
+            {
+                CountChar++;
+            }
+        }
+        return CountChar;
+    }
     public int Add(String Numbers)
     {
         count++;
@@ -48,7 +61,24 @@ public class StringCalculator
         String delimiter="[,\n]";
         if(Numbers.startsWith("//"))
         {
-            if(Numbers.startsWith("//["))
+            int DelimiterEnd=Numbers.indexOf("\n");
+            String DelimiterSection=Numbers.substring(2,DelimiterEnd);
+            int CountChar=CountChar(Numbers,'[');
+            if(CountChar>1) {
+                if (Numbers.contains("[") && Numbers.contains("]")) {
+                    ArrayList<String> delimiters = new ArrayList<>();
+                    Matcher matcher = Pattern.compile("\\[(.)\\]").matcher(DelimiterSection);
+                    while (matcher.find()) {
+                        delimiters.add(Pattern.quote(matcher.group(1)));
+                    }
+                    delimiter = String.join("|", delimiters);
+                }
+                Numbers = Numbers.substring(DelimiterEnd + 1);
+
+            }
+            else
+            {
+                if(Numbers.startsWith("//["))
             {
                 int endIdx=Numbers.indexOf("]\n");
                 String AnyNumberOfDelimiter=Numbers.substring(3,endIdx);
@@ -60,6 +90,7 @@ public class StringCalculator
                 String CustomDelimiter = Numbers.substring(2, IdxOfNl);
                 delimiter = Pattern.quote(CustomDelimiter);
                 Numbers = Numbers.substring(IdxOfNl + 1);
+            }
             }
 
         }
